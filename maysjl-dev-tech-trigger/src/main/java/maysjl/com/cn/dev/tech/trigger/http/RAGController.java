08f3091fcc2b6b0ec4112db669837c951f580cc0
linlin20ai <3357929671@qq.com>
2025-05-16 22:56:48 +0800

    FEAT: RAG GIT代码仓库解析到知识库

--- maysjl-dev-tech-trigger/src/main/java/maysjl/com/cn/dev/tech/trigger/http/RAGController.java
+++ maysjl-dev-tech-trigger/src/main/java/maysjl/com/cn/dev/tech/trigger/http/RAGController.java
@@ -1,21 +1,31 @@
 package maysjl.com.cn.dev.tech.trigger.http;
 
+
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import maysjl.com.cn.dev.tech.api.IRAGService;
 import maysjl.com.cn.dev.tech.api.reponse.Response;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.redisson.api.RList;
 import org.redisson.api.RedissonClient;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.PgVectorStore;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.core.io.PathResource;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.ai.document.Document;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
+
 
 /**
  * @program: AI-RAG
@@ -50,7 +60,6 @@ public class RAGController implements IRAGService {
                 .build();
     }
 
-
     @RequestMapping(value = "file/upload", method = RequestMethod.POST, headers = "content-type=multipart/form-data")
     @Override
     public Response<String> uploadFile(@RequestParam String ragTag, @RequestParam("file") List<MultipartFile> files) {
@@ -66,7 +75,7 @@ public class RAGController implements IRAGService {
             pgVectorStore.accept(documentSplitterList);
 
             RList<String> elements = redissonClient.getList("ragTag");
-            if (!elements.contains(ragTag)){
+            if (!elements.contains(ragTag)) {
                 elements.add(ragTag);
             }
         }
@@ -74,5 +83,70 @@ public class RAGController implements IRAGService {
         log.info("上传知识库完成 {}", ragTag);
         return Response.<String>builder().code("0000").info("调用成功").build();
     }
+
+    @RequestMapping(value = "analyze_git_repository", method = RequestMethod.POST)
+    @Override
+    public Response<String> analyzeGitRepository(@RequestParam String repoUrl, @RequestParam String userName, @RequestParam String token) throws Exception {
+        String localPath = "./git-cloned-repo";
+        String repoProjectName = extractProjectName(repoUrl);
+        log.info("克隆路径：{}", new File(localPath).getAbsolutePath());
+
+        FileUtils.deleteDirectory(new File(localPath));
+
+        Git git = Git.cloneRepository()
+                .setURI(repoUrl)
+                .setDirectory(new File(localPath))
+                .setCredentialsProvider(new UsernamePasswordCredentialsProvider(userName, token))
+                .call();
+
+        Files.walkFileTree(Paths.get(localPath), new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                log.info("{} 遍历解析路径，上传知识库:{}", repoProjectName, file.getFileName());
+                try {
+                    TikaDocumentReader reader = new TikaDocumentReader(new PathResource(file));
+                    List<Document> documents = reader.get();
+                    List<Document> documentSplitterList = tokenTextSplitter.apply(documents);
+
+                    documents.forEach(doc -> doc.getMetadata().put("knowledge", repoProjectName));
+
+                    documentSplitterList.forEach(doc -> doc.getMetadata().put("knowledge", repoProjectName));
+
+                    pgVectorStore.accept(documentSplitterList);
+                } catch (Exception e) {
+                    log.error("遍历解析路径，上传知识库失败:{}", file.getFileName());
+                }
+
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                log.info("Failed to access file: {} - {}", file.toString(), exc.getMessage());
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        FileUtils.deleteDirectory(new File(localPath));
+
+        RList<String> elements = redissonClient.getList("ragTag");
+        if (!elements.contains(repoProjectName)) {
+            elements.add(repoProjectName);
+        }
+
+        git.close();
+
+        log.info("遍历解析路径，上传完成:{}", repoUrl);
+
+        return Response.<String>builder().code("0000").info("调用成功").build();
+    }
+
+    private String extractProjectName(String repoUrl) {
+        String[] parts = repoUrl.split("/");
+        String projectNameWithGit = parts[parts.length - 1];
+        return projectNameWithGit.replace(".git", "");
+    }
+
+
 
 }
